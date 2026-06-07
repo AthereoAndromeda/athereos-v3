c1c75a26030f6d6affe7e11d256dbfde8e253e74
Athereo <athereoandromeda@gmail.com>
2026-06-07 16:22:26 +0800

    feat(impermanence): Add systemd rollback unit

--- modules/config/impermanence.nix
+++ modules/config/impermanence.nix
@@ -3,6 +3,43 @@
     nixos = {user, ...}: {
       imports = [inputs.impermanence.nixosModules.impermanence];
 
+      boot.initrd.systemd = {
+        enable = true;
+        services.rollback = {
+          description = "Rollback BTRFS subvolume to a pristine state";
+          wantedBy = ["initrd.target"];
+
+          # LUKS/TPM process
+          after = ["systemd-cryptsetup@enc.service"];
+
+          # Before mounting the system root (/sysroot) during early boot process
+          before = ["sysroot.mount"];
+
+          unitConfig.DefaultDependencies = "no";
+          serviceConfig.Type = "oneshot";
+          script = ''
+            mkdir -p /mnt
+
+            mount -o subvol=/ /dev/mapper/enc /mnt
+
+            # Recursively remove subvolumes
+            btrfs subvolume list -o /mnt/root |
+              cut -f9 -d' ' |
+              while read subvolume; do
+                echo "Deleting /$subvolume subvolume..."
+                btrfs subvolume delete "/mnt/$subvolume"
+              done &&
+              echo "Deleting /root subvolume..." &&
+              btrfs subvolume delete /mnt/root
+            echo "Restoring blank /root subvolume..."
+            btrfs subvolume snapshot /mnt/root-blank /mnt/root
+
+            # Once done rolling back, we can unmount
+            umount /mnt
+          '';
+        };
+      };
+
       environment.persistence."/persist" = {
         enable = true;
         hideMounts = true;
@@ -12,6 +49,7 @@
           "/var/lib/bluetooth"
           "/var/lib/nixos"
           "/var/lib/systemd/coredump"
+          "/var/db/sudo"
           # "/var/lib/cups"
           # "/var/lib/greetd"
           # "/var/lib/regreet"
@@ -24,6 +62,7 @@
 
         files = [
           "/etc/machine-id"
+          "/etc/NIXOS" # Empty file marker
           # "/etc/ssh/ssh_known_hosts"
         ];
 
