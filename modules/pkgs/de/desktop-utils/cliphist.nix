6aa6c5bbcebc43dc9d24ff53071ae87ce82e6297
Athereo <athereoandromeda@gmail.com>
2026-06-07 16:30:46 +0800

    fix(cliphist): Enable `wl-clipboard`

--- modules/pkgs/de/desktop-utils/cliphist.nix
+++ modules/pkgs/de/desktop-utils/cliphist.nix
@@ -1,5 +1,8 @@
 {...}: {
   den.aspects.desktop.cliphist = {
+    nixos = {pkgs, ...}: {
+      environment.systemPackages = [pkgs.wl-clipboard-rs];
+    };
     homeManager.services.cliphist.enable = true;
   };
 }
