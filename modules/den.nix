759dbae86d49a69fb10653588916e21cab134140
Athereo <athereoandromeda@gmail.com>
2026-06-08 00:19:37 +0800

    feat(nix): Upgrade to `Lix`

--- modules/den.nix
+++ modules/den.nix
@@ -29,6 +29,19 @@
   };
 
   den.default.nixos = {pkgs, ...}: {
+    nixpkgs.overlays = [
+      (final: prev: {
+        inherit
+          (prev.lixPackageSets.stable)
+          nixpkgs-review
+          nix-eval-jobs
+          nix-fast-build
+          colmena
+          ;
+      })
+    ];
+
+    nix.package = pkgs.lixPackageSets.stable.lix;
     home-manager = {
       useGlobalPkgs = true;
       useUserPackages = true;
