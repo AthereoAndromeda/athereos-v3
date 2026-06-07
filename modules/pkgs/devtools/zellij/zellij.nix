ebda5f4c4669d3ea575f49871b395d2e181e44a6
Athereo <athereoandromeda@gmail.com>
2026-06-08 01:09:09 +0800

    pkg(zellij): Add `config.kdl`

--- modules/pkgs/devtools/zellij/zellij.nix
+++ modules/pkgs/devtools/zellij/zellij.nix
@@ -2,6 +2,8 @@
   dev-tools.zellij.homeManager = {
     programs.zellij = {
       enable = true;
+      enableBashIntegration = true;
+      extraConfig = builtins.readFile ./config.kdl;
     };
   };
 }
