0d0f3b293ca717344a54f4eb376e1e71e71d33c8
Athereo <athereoandromeda@gmail.com>
2026-07-12 20:20:10 +0800

    fix(xdg): Fix `xdg-open`

--- modules/config/xdg.nix
+++ modules/config/xdg.nix
@@ -1,5 +1,33 @@
 {...}: {
-  den.aspects.xdg-utils = {
+  den.aspects.xdg-utils = let
+    common-config = {
+      default = ["gtk"];
+
+      # Use the 'wlr' portal for screen sharing/specific wayland tasks
+      "org.freedesktop.impl.portal.Screenshot" = ["wlr"];
+      "org.freedesktop.impl.portal.ScreenCast" = ["wlr"];
+      "org.freedesktop.impl.portal.Secret" = ["gnome-keyring"];
+    };
+  in {
+    nixos = {pkgs, ...}: {
+      xdg.portal = {
+        enable = true;
+        wlr.enable = true;
+
+        xdgOpenUsePortal = true;
+        extraPortals = with pkgs; [
+          xdg-desktop-portal-gtk
+          xdg-desktop-portal-wlr
+          # xdg-desktop-portal-termfilechooser
+        ];
+
+        config = {
+          common = common-config;
+          niri = common-config;
+        };
+      };
+    };
+
     homeManager = {pkgs, ...}: {
       xdg = {
         enable = true;
@@ -8,21 +36,14 @@
           enable = true;
           xdgOpenUsePortal = true;
           extraPortals = with pkgs; [
-            xdg-desktop-portal
             xdg-desktop-portal-gtk
             xdg-desktop-portal-wlr
             # xdg-desktop-portal-termfilechooser
           ];
 
           config = {
-            common = {
-              default = ["gtk"];
-
-              # Use the 'wlr' portal for screen sharing/specific wayland tasks
-              "org.freedesktop.impl.portal.Screenshot" = ["wlr"];
-              "org.freedesktop.impl.portal.ScreenCast" = ["wlr"];
-              "org.freedesktop.impl.portal.Secret" = ["gnome-keyring"];
-            };
+            common = common-config;
+            niri = common-config;
           };
         };
 
