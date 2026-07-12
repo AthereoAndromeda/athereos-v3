da3f23bcf38c766f0a4e4727e44168286aad8f29
Athereo <athereoandromeda@gmail.com>
2026-07-12 20:37:40 +0800

    fix(bash): Enable bash in home-manager

--- modules/pkgs/shells/shells.nix
+++ modules/pkgs/shells/shells.nix
@@ -3,6 +3,8 @@
     includes = [den.aspects.pkgs.shells.nushell];
 
     homeManager = {...}: {
+      programs.bash.enable = true;
+      home.shell.enableBashIntegration = true;
       home.shellAliases = {
         j = "just";
         ff = "fastfetch";
