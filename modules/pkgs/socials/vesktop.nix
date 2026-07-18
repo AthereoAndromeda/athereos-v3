f67aa41650310df62971b34a644968bae2441fbd
Athereo <athereoandromeda@gmail.com>
2026-07-18 17:13:30 +0800

    chore(nix): Update flake

--- modules/pkgs/socials/vesktop.nix
+++ modules/pkgs/socials/vesktop.nix
@@ -1,10 +1,15 @@
 {inputs, ...}: {
   den.aspects.pkgs.vesktop = {
+    nixos = {
+      nixpkgs.config.permittedInsecurePackages = [
+        "electron-40.10.5"
+      ];
+    };
     homeManager = {host, ...}: let
       pinned-pkgs = import inputs.pinned-nixpkgs {inherit (host) system;};
     in {
       programs.vesktop.enable = true;
-      programs.vesktop.package = pinned-pkgs.vesktop;
+      # programs.vesktop.package = pinned-pkgs.vesktop;
     };
   };
 }
