deabccac70daf2b4e17389d0190bed4234e98908
Athereo <athereoandromeda@gmail.com>
2026-08-16 20:45:44 +0800

    feat(yazi): Add plugins and stylize

---
+++ modules/pkgs/cli-tools/yazi/yazi.nix
@@ -0,0 +1,33 @@
+{lib, ...}: {
+  den.aspects.pkgs.yazi = {
+    nixos = {pkgs, ...}: {
+      environment.systemPackages = [(pkgs.yazi.override {_7zz = pkgs._7zz-rar;})];
+    };
+
+    homeManager = {pkgs, ...}: {
+      programs.yazi = {
+        package = pkgs.yazi.override {_7zz = pkgs._7zz-rar;};
+        enable = true;
+        enableBashIntegration = true;
+        enableNushellIntegration = true;
+        shellWrapperName = "y";
+
+        initLua = builtins.readFile ./init.lua;
+
+        plugins = {
+          inherit
+            (pkgs.yaziPlugins)
+            git
+            chmod
+            yatline
+            compress
+            ;
+        };
+
+        keymap = lib.fromTOML (builtins.readFile ./keymap.toml);
+        settings = lib.fromTOML (builtins.readFile ./yazi.toml);
+        theme.flavor.dark = "tokyo-night";
+      };
+    };
+  };
+}
