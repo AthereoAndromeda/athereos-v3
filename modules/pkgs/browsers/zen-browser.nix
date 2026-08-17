a7c24fb89635a2b83344749d7e7d1f86003cdbde
Athereo <athereoandromeda@gmail.com>
2026-08-17 21:07:17 +0800

    fix(zen-beta): Set mime associations

--- modules/pkgs/browsers/zen-browser.nix
+++ modules/pkgs/browsers/zen-browser.nix
@@ -6,6 +6,14 @@
         enable = true;
         nativeMessagingHosts = [pkgs.firefoxpwa];
       };
+
+      xdg.mimeApps.defaultApplications = {
+        "text/html" = "zen-beta.desktop";
+        "x-scheme-handler/http" = "zen-beta.desktop";
+        "x-scheme-handler/https" = "zen-beta.desktop";
+        "x-scheme-handler/about" = "zen-beta.desktop";
+        "x-scheme-handler/unknown" = "zen-beta.desktop";
+      };
     };
 
     persist.home.directories = [
