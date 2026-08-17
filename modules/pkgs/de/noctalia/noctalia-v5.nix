ce6704edc16a2b841082c52faa813a976ad91e5a
Athereo <athereoandromeda@gmail.com>
2026-08-17 18:17:13 +0800

    feat(noctalia): Add plugins and services

--- modules/pkgs/de/noctalia/noctalia-v5.nix
+++ modules/pkgs/de/noctalia/noctalia-v5.nix
@@ -3,34 +3,68 @@
   den,
   ...
 }: {
-  den.aspects.noctalia-v5.includes = [den.aspects.noctalia-greeter];
-  den.aspects.noctalia-v5.persist.home.directories = [".local/state/noctalia"];
+  den.aspects.noctalia-v5 = {
+    includes = [den.aspects.noctalia-greeter];
+    persist.home.directories = [".local/state/noctalia"];
 
-  den.aspects.noctalia-v5.nixos = {pkgs, ...}: {
-    nix.settings = {
-      extra-substituters = ["https://noctalia.cachix.org"];
-      extra-trusted-public-keys = ["noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="];
+    nixos = {
+      pkgs,
+      config,
+      ...
+    }: {
+      warnings = [
+        {
+          assertion = config.services.udisks2.enable;
+          message = "Noctalia plugins require udisks service.";
+        }
+        {
+          assertion = config.programs.kdeconnect.enable;
+          message = "Noctalia plugins require KDE Connect.";
+        }
+      ];
+
+      nix.settings = {
+        extra-substituters = ["https://noctalia.cachix.org"];
+        extra-trusted-public-keys = ["noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="];
+      };
+
+      imports = [
+        inputs.noctalia-v5.nixosModules.default
+      ];
+
+      qt.enable = true;
+      services.upower.enable = true;
+
+      environment.systemPackages =
+        [
+          inputs.noctalia-v5.packages.${pkgs.stdenv.hostPlatform.system}.default
+        ]
+        ++ (with pkgs; [
+          udiskie
+          udisks2
+        ]);
+
+      services.udisks2 = {
+        enable = true;
+        mountOnMedia = true;
+      };
+
+      programs.kdeconnect.enable = true;
+
+      networking.firewall = rec {
+        allowedTCPPortRanges = [
+          {
+            from = 1714;
+            to = 1764;
+          }
+        ];
+        allowedUDPPortRanges = allowedTCPPortRanges;
+      };
+
+      programs.noctalia = {
+        enable = true;
+        recommendedServices.enable = true;
+      };
     };
-
-    imports = [
-      inputs.noctalia-v5.nixosModules.default
-    ];
-
-    qt.enable = true;
-    services.upower.enable = true;
-    # services.tuned.enable = true;
-
-    environment.systemPackages = [
-      inputs.noctalia-v5.packages.${pkgs.stdenv.hostPlatform.system}.default
-    ];
-
-    programs.noctalia = {
-      enable = true;
-      recommendedServices.enable = true;
-    };
-  };
-
-  den.aspects.noctalia-v5.homeManager = {...}: {
-    # imports = [inputs.noctalia-v5.homeModules.default];
   };
 }
