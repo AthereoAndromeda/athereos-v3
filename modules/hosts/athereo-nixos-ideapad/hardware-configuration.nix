c232ce2a6b655b7ad920bd97455200de169af5e0
Athereo <athereoandromeda@gmail.com>
2026-08-17 15:35:05 +0800

    feat(swap): Encrypt swap and use `zswap`

--- modules/hosts/athereo-nixos-ideapad/hardware-configuration.nix
+++ modules/hosts/athereo-nixos-ideapad/hardware-configuration.nix
@@ -1,8 +1,13 @@
 {lib, ...}: {
   den.aspects.hardware.athereo-nixos-ideapad = {
     nixos = {config, ...}: {
-      boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "usb_storage" "sd_mod" "sdhci_pci"];
-      boot.initrd.kernelModules = [];
+      boot.initrd = {
+        availableKernelModules = ["nvme" "xhci_pci" "usb_storage" "sd_mod" "sdhci_pci"];
+        kernelModules = [];
+
+        luks.devices."enc".device = "/dev/disk/by-uuid/c56d902e-d1b7-4803-b754-1f4d451b1c5f";
+      };
+
       boot.kernelModules = ["kvm-amd"];
       boot.extraModulePackages = [];
       boot.supportedFilesystems = {
@@ -10,19 +15,24 @@
         ntfs = true;
       };
 
+      boot.zswap = {
+        enable = true;
+      };
+
+      # Here a more complete example
+      boot.kernelParams = [
+        "zswap.enabled=1"
+        "zswap.max_pool_percent=50"
+        "zswap.shrinker_enabled=1"
+        "zswap.compressor=zstd"
+        "zswap.accept_threshold_percent=90"
+      ];
+
       fileSystems."/" = {
         device = "/dev/mapper/enc";
         fsType = "btrfs";
         options = ["subvol=root" "compress=zstd" "noatime"];
       };
-
-      boot.initrd.luks.devices."enc".device = "/dev/disk/by-uuid/c56d902e-d1b7-4803-b754-1f4d451b1c5f";
-
-      # fileSystems."/home" = {
-      #   device = "/dev/mapper/enc";
-      #   fsType = "btrfs";
-      #   options = ["subvol=home" "compress=zstd" "noatime"];
-      # };
 
       fileSystems."/nix" = {
         device = "/dev/mapper/enc";
@@ -51,7 +61,15 @@
       };
 
       swapDevices = [
-        {device = "/dev/disk/by-uuid/ad39123d-0e0c-40e2-ade9-9829a50e066f";}
+        {
+          device = "/dev/disk/by-label/lb_swap";
+          options = ["discard"];
+          encrypted = {
+            enable = true;
+            label = "swap";
+            blkDev = "/dev/disk/by-label/lb_luks_swap";
+          };
+        }
       ];
 
       nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
