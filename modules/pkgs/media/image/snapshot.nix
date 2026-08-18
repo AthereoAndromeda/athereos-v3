6823b61a7a0c103083ccfa2c1f43048d55eb552c
Athereo <athereoandromeda@gmail.com>
2026-08-18 14:04:35 +0800

    test(snapshot): Add assertion for `snapshot`

--- modules/pkgs/media/image/snapshot.nix
+++ modules/pkgs/media/image/snapshot.nix
@@ -1,7 +1,18 @@
 {...}: {
   den.aspects.pkgs.snapshot = {
-    homeManager = {pkgs, ...}: {
+    homeManager = {
+      pkgs,
+      config,
+      ...
+    }: {
       home.packages = [pkgs.snapshot];
+
+      assertions = [
+        {
+          assertion = config.xdg.userDirs.enable;
+          message = "Snapshot will fail to capture if $XDG_CONFIG_DIRS/user-dirs.dirs does not exist";
+        }
+      ];
     };
   };
 }
