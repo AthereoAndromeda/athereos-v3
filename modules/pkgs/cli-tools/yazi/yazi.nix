308fe36d5545de325680dfcf85376f569fd9941a
Athereo <athereoandromeda@gmail.com>
2026-08-19 19:49:59 +0800

    fix(yazi): Add `dracula` flavor

--- modules/pkgs/cli-tools/yazi/yazi.nix
+++ modules/pkgs/cli-tools/yazi/yazi.nix
@@ -4,7 +4,26 @@
       environment.systemPackages = [(pkgs.yazi.override {_7zz = pkgs._7zz-rar;})];
     };
 
-    homeManager = {pkgs, ...}: {
+    homeManager = {pkgs, ...}: let
+      dracula-pkg = pkgs.stdenvNoCC.mkDerivation {
+        name = "dracula";
+
+        src = fetchGit {
+          url = "https://github.com/yazi-rs/flavors.git";
+          rev = "be0b21d0873092a63946cc2678dd700aac945902";
+        };
+
+        phases = ["unpackPhase" "installPhase"];
+        installPhase = ''
+          runHook preInstall
+
+          mkdir -p $out
+          cp -r $src/dracula.yazi/* $out
+
+          runHook postInstall
+        '';
+      };
+    in {
       programs.yazi = {
         package = pkgs.yazi.override {_7zz = pkgs._7zz-rar;};
         enable = true;
@@ -27,6 +46,10 @@
 
         keymap = lib.fromTOML (builtins.readFile ./keymap.toml);
         settings = lib.fromTOML (builtins.readFile ./yazi.toml);
+
+        flavors = {
+          dracula = dracula-pkg;
+        };
         theme.flavor.dark = "dracula";
       };
     };
