2b5c0c0b2b746b3acb6f09de3868f9570a6e5fb2
Athereo <athereoandromeda@gmail.com>
2026-08-23 22:21:09 +0800

    refactor(just): Move `just`

--- modules/pkgs/dev-tools/just.nix
+++ modules/pkgs/dev-tools/just.nix
@@ -1,4 +1,6 @@
-{...}: {
+{den, ...}: {
+  dev-tools.just = den.aspects.pkgs.just;
+
   den.aspects.pkgs.just = {
     nixos = {pkgs, ...}: {
       environment.systemPackages = [pkgs.just pkgs.just-lsp];
