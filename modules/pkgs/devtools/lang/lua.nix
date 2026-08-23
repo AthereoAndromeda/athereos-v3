362e691bfba02472d42b5f1be8785d4c056b10b7
Athereo <athereoandromeda@gmail.com>
2026-08-23 20:05:12 +0800

    feat(devtools/lua): Add `lua` and `luau`

---
+++ modules/pkgs/devtools/lang/lua.nix
@@ -0,0 +1,13 @@
+{...}: {
+  devtools.lua = {
+    nixos = {pkgs, ...}: {
+      environment.systemPackages = with pkgs; [
+        lua
+        lua-language-server
+
+        luau
+        luau-lsp
+      ];
+    };
+  };
+}
