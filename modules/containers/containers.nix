3f6b695280fbe1fb1efc59be86dd036d0861ac33
AthereoAndromeda <athereoandromeda@gmail.com>
2026-08-28 23:40:55 +0800

    fix(containers): Add support for `iptables`

--- modules/containers/containers.nix
+++ modules/containers/containers.nix
@@ -1,17 +1,12 @@
-{...}: {
+{lib, ...}: {
   den.aspects.containers = {
     nixos = {config, ...}: {
-      assertions = [
-        {
-          assertion = config.networking.nftables.enable;
-          message = "nftables must be used.";
-        }
-      ];
-
       networking.nat = {
         enable = true;
-        internalInterfaces = ["ve-*"];
-        # externalInterface = "ens3";
+        internalInterfaces = lib.mkMerge [
+          (lib.mkIf config.networking.nftables.enable ["ve-*"])
+          (lib.mkIf (!config.networking.nftables.enable) ["ve-+"])
+        ];
         externalInterface = "wlp2s0";
         enableIPv6 = true;
       };
