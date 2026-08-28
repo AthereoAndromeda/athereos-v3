344e6d9d7c9cd49538d44a83ae98eb68c68d0987
Athereo <athereoandromeda@gmail.com>
2026-08-29 01:22:58 +0800

    fix(ssh): Fix ssh config

--- modules/pkgs/security/ssh.nix
+++ modules/pkgs/security/ssh.nix
@@ -4,6 +4,38 @@
       services.openssh.enable = true;
     };
 
+    homeManager = {
+      programs.ssh = {
+        enable = true;
+        enableDefaultConfig = false;
+
+        settings = {
+          # Default settings
+          "*" = {
+            ForwardAgent = false;
+            AddKeysToAgent = "no";
+            Compression = false;
+            ServerAliveInterval = 0;
+            ServerAliveCountMax = 3;
+            HashKnownHosts = false;
+            UserKnownHostsFile = "~/.ssh/known_hosts";
+            ControlMaster = "no";
+            ControlPath = "~/.ssh/master-%r@%n:%p";
+            ControlPersist = "no";
+          };
+
+          "gitlab.com github.com codeberg.org" = {
+            # Host = "gitlab.com github.com codeberg.org";
+            IdentitiesOnly = true;
+            IdentityFile = [
+              "~/.ssh/id_ed25519"
+              "~/.ssh/github_signing"
+            ];
+          };
+        };
+      };
+    };
+
     persist.files = [
       "/etc/ssh/ssh_host_ed25519_key"
       "/etc/ssh/ssh_host_ed25519_key.pub"
@@ -14,7 +46,7 @@
     persist.home.directories = [
       {
         directory = ".ssh";
-        mode = "0600";
+        mode = "0700";
       }
     ];
   };
