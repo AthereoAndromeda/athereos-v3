ca00126a73afe2707d5961d23f4ac288701132e0
Athereo <athereoandromeda@gmail.com>
2026-08-30 18:53:31 +0800

    refactor(nix): Refactor `nix-settings.nix`

---
+++ modules/config/nix-settings.nix
@@ -0,0 +1,44 @@
+{lib, ...}: {
+  den.aspects.nix-settings = {
+    nixos = {config, ...}: {
+      nix.settings.trusted-users = ["root" "@wheel"];
+
+      sops.secrets."nix-settings/access-tokens/github" = {
+        reloadUnits = ["nix-daemon.service"];
+        owner = "root";
+        group = "root";
+        mode = "0400";
+      };
+
+      sops.templates."nix-access-tokens.conf" = let
+        access-token-content = tokens: "access-token = ${lib.concatStringsSep " " tokens}";
+      in {
+        content = access-token-content [config.sops.placeholder."nix-settings/access-tokens/github"];
+      };
+
+      nix.extraOptions = ''
+        !include ${config.sops.templates."nix-access-tokens.conf".path}
+      '';
+
+      nix.settings.experimental-features = ["nix-command" "flakes"];
+
+      # Hardware-optimized for v3
+      nix.settings.system-features = [
+        "nixos-test"
+        "benchmark"
+        "big-parallel"
+        "kvm"
+        "gccarch-znver3"
+        "gccarch-x86-64-v3"
+        "gccarch-x86-64-v2"
+        "gccarch-x86-64"
+      ];
+
+      # nixpkgs.hostPlatform = {
+      #   system = "x86_64-linux";
+      #   gcc.arch = "znver3";
+      #   gcc.tune = "znver3";
+      # };
+    };
+  };
+}
--- modules/den.nix
+++ modules/den.nix
@@ -16,6 +16,7 @@
 
   den.default.includes = with den.aspects;
     [
+      nix-settings
       lix
       unfree
       hardware-utils
@@ -47,11 +48,7 @@
     home.stateVersion = "26.05";
   };
 
-  den.default.nixos = {
-    pkgs,
-    config,
-    ...
-  }: {
+  den.default.nixos = {pkgs, ...}: {
     nixpkgs.overlays = [
       inputs.nuenv.overlays.default
     ];
@@ -138,42 +135,5 @@
     system.stateVersion = "26.05"; # Did you read the comment?
 
     system.configurationRevision = self.rev or self.dirtyRev or null;
-    nix.settings.trusted-users = ["root" "@wheel"];
-
-    sops.secrets."nix-settings/access-tokens/github" = {
-      reloadUnits = ["nix-daemon.service"];
-      owner = "root";
-      group = "root";
-      mode = "0400";
-    };
-
-    sops.templates."nix-access-tokens.conf" = let
-      access-token-content = tokens: "access-token = ${lib.concatStringsSep " " tokens}";
-    in {
-      content = access-token-content [config.sops.placeholder."nix-settings/access-tokens/github"];
-    };
-
-    nix.extraOptions = ''
-      !include ${config.sops.templates."nix-access-tokens.conf".path}
-    '';
-
-    nix.settings.experimental-features = ["nix-command" "flakes"];
-    # Hardware-optimized for v3
-    nix.settings.system-features = [
-      "nixos-test"
-      "benchmark"
-      "big-parallel"
-      "kvm"
-      "gccarch-znver3"
-      "gccarch-x86-64-v3"
-      "gccarch-x86-64-v2"
-      "gccarch-x86-64"
-    ];
-
-    # nixpkgs.hostPlatform = {
-    #   system = "x86_64-linux";
-    #   gcc.arch = "znver3";
-    #   gcc.tune = "znver3";
-    # };
   };
 }
