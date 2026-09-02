109897fe3560c895d9301338b4bb1ef674e1cd97
Athereo <athereoandromeda@gmail.com>
2026-09-02 18:33:28 +0800

    refactor(pkgs/thunderbird): Refactor `mimeApps`

--- modules/pkgs/office/thunderbird.nix
+++ modules/pkgs/office/thunderbird.nix
@@ -1,21 +1,21 @@
-{...}: {
+{lib, ...}: {
   den.aspects.pkgs.thunderbird = {
     homeManager = {
       programs.thunderbird = {
         enable = true;
       };
 
-      xdg.mimeApps.defaultApplications = {
-        "x-scheme-handler/mailto" = "thunderbird.desktop";
-        "x-scheme-handler/feed" = "thunderbird.desktop";
-        "x-scheme-handler/news" = "thunderbird.desktop";
-        "x-scheme-handler/net.thunderbird" = "thunderbird.desktop";
-        "application/rss+xml" = "thunderbird.desktop";
-        "application/atom+xml" = "thunderbird.desktop";
-        "application/rdf+xml" = "thunderbird.desktop";
-        "application/x-extension-rss" = "thunderbird.desktop";
-        "application/x-xpinstall" = "thunderbird.desktop";
-      };
+      xdg.mimeApps.defaultApplications = lib.genAttrs [
+        "x-scheme-handler/mailto"
+        "x-scheme-handler/feed"
+        "x-scheme-handler/news"
+        "x-scheme-handler/net.thunderbird"
+        "application/rss+xml"
+        "application/atom+xml"
+        "application/rdf+xml"
+        "application/x-extension-rss"
+        "application/x-xpinstall"
+      ] (_: "thunderbird.desktop");
     };
 
     persist.home.directories = [
