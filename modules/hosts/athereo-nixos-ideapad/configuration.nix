9679a6d944440e54f1b5156fac5b80da513fa5c4
Athereo <athereoandromeda@gmail.com>
2026-09-06 00:06:51 +0800

    feat(tpm2): Add `tpm2` support

--- modules/hosts/athereo-nixos-ideapad/configuration.nix
+++ modules/hosts/athereo-nixos-ideapad/configuration.nix
@@ -10,12 +10,20 @@
       scripts.lenovoctl
     ];
 
+    users.extraGroups = ["tss"];
+
     nixos = {pkgs, ...}: {
       imports = [inputs.nixos-hardware.nixosModules.lenovo-ideapad-16ahp9];
 
       services.udev.extraRules = ''
         ACTION=="add|change", SUBSYSTEM=="platform", DRIVER=="ideapad_acpi", RUN+="${pkgs.coreutils}/bin/chgrp lenovoctl /sys%p/conservation_mode", RUN+="${pkgs.coreutils}/bin/chmod 664 /sys%p/conservation_mode"
       '';
+
+      security.tpm2 = {
+        enable = true;
+        pkcs11.enable = true;
+        tctiEnvironment.enable = true;
+      };
     };
 
     provides.to-users = {user, ...}: {
