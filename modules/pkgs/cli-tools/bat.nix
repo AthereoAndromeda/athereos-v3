ae07239b44cf85d7d0fcc9233b518f786ca348b6
Athereo <athereoandromeda@gmail.com>
2026-09-09 01:48:59 +0800

    feat(pkgs/bat): Enable italics

--- modules/pkgs/cli-tools/bat.nix
+++ modules/pkgs/cli-tools/bat.nix
@@ -1,16 +1,18 @@
 {...}: {
   den.aspects.pkgs.bat = let
-    bat-common = pkgs: {
+  in {
+    nixos = {pkgs, ...}: {
       programs.bat.enable = true;
       programs.bat.extraPackages = with pkgs.bat-extras; [
         batdiff
         batman
         prettybat
       ];
-    };
-  in {
-    nixos = {pkgs, ...}: bat-common pkgs;
 
-    homeManager = {pkgs, ...}: bat-common pkgs;
+      programs.bat.settings = {
+        italic-text = "always";
+        theme = "noctalia";
+      };
+    };
   };
 }
